aceea9e137260321579c69276c2f3abbfb683872
Veronika Savluk <s_veronica@ukr.net>
2022-07-07 12:06:16 +0300

    task 4

---
+++ src/js/task-04.js
@@ -0,0 +1,19 @@
+// Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
+// Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
+// Оновлюй інтерфейс новим значенням змінної counterValue.
+
+let counterValue = 0;
+
+const addValueBtn = document.querySelector("[data-action=increment]");
+const removeValueBtn = document.querySelector("[data-action=decrement]");
+const counterValueEl = document.querySelector("span#value");
+
+addValueBtn.addEventListener("click", () => {
+    counterValue += Number(addValueBtn.textContent);
+    counterValueEl.textContent = counterValue;
+})
+
+removeValueBtn.addEventListener("click", () => {
+    counterValue += Number(removeValueBtn.textContent);
+    counterValueEl.textContent = counterValue;
+})
