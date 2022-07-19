75960e1af66fe0c43aacaf549a9ca01fd92e6abe
Veronika Savluk <s_veronica@ukr.net>
2022-07-19 22:49:07 +0300

    improvment task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,11 +1,19 @@
 const listCategories = document.querySelectorAll('li.item');
 console.log(`Number of categories: ${listCategories.length}`);
-for (let i = 0; i < listCategories.length; i += 1) {
-    // console.log(listCategories[i]);
-    const categoryName = listCategories[i].querySelector(`h2`);
+
+// for (let i = 0; i < listCategories.length; i += 1) {
+//     // console.log(listCategories[i]);
+//     const categoryName = listCategories[i].querySelector(`h2`);
+//     console.log(`Category: ${categoryName.textContent}`);
+//     const elements = listCategories[i].querySelectorAll(`li`);
+//     console.log(`Elements: ${elements.length}`);
+// }
+listCategories.forEach((item) => {
+    const categoryName = item.firstElementChild;
     console.log(`Category: ${categoryName.textContent}`);
-    const elements = listCategories[i].querySelectorAll(`li`);
-    console.log(`Elements: ${elements.length}`);
-}
+
+    const elements = item.lastElementChild;
+    console.log(`Elements: ${elements.children.length}`);
+})
 
 
