4c8ba7f0a45e6c91738a9d97afb7bfab2683124b
Veronika Savluk <s_veronica@ukr.net>
2022-07-19 22:49:44 +0300

    improvment task 4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -8,12 +8,20 @@ const addValueBtn = document.querySelector("[data-action=increment]");
 const removeValueBtn = document.querySelector("[data-action=decrement]");
 const counterValueEl = document.querySelector("span#value");
 
-addValueBtn.addEventListener("click", () => {
-    counterValue += Number(addValueBtn.textContent);
-    counterValueEl.textContent = counterValue;
-})
+// addValueBtn.addEventListener("click", () => {
+//     counterValue += Number(addValueBtn.textContent);
+//     counterValueEl.textContent = counterValue;
+// });
 
-removeValueBtn.addEventListener("click", () => {
-    counterValue += Number(removeValueBtn.textContent);
+// removeValueBtn.addEventListener("click", () => {
+//     counterValue += Number(removeValueBtn.textContent);
+//     counterValueEl.textContent = counterValue;
+// });
+
+const handleClick = (event) => {
+    counterValue += Number(event.target.textContent);
     counterValueEl.textContent = counterValue;
-})
+};
+
+addValueBtn.addEventListener("click", handleClick);
+removeValueBtn.addEventListener("click", handleClick);
